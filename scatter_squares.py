d1d538ed79a3cff63ef97883a9d0a340d12e5661
luye11 <787426137@qq.com>
2018-03-07 17:08:32 +0800

    Initial commit

---
+++ scatter_squares.py
@@ -0,0 +1,20 @@
+# -*- coding:UTF-8-*-
+import matplotlib.pyplot as plt
+
+x_values=list(range(1,1001))
+y_values=[x**2 for x in x_values]
+
+plt.scatter(x_values,y_values,c=y_values,cmap=plt.cm.Blues,edgecolors='none',s=40)
+
+#设置图标标题并给坐标轴加上标签
+plt.title("Square Numbers",fontsize=24)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of Value",fontsize=14)
+
+#设置刻度标记的大小
+plt.tick_params(axis='both',which='major',labelsize=14)
+
+plt.axis([0,1100,0,1100000])
+
+#plt.show()
+plt.savefig('squares_plot2.png',bbox_inches='tight')
